c09f17bf341ee10246fc875fa5f8d7be79260fbb
Ross McFadyen <ross.mcfadyen@bbc.co.uk>
2018-07-03 15:52:53 +0100

    affd

--- getPodcastCommand.js
+++ getPodcastCommand.js
@@ -4,23 +4,30 @@ const podcasts = [
   {
     podcastId: "theArchersId",
     podcastName: "The Archers",
-    podcastStreamUrl: "http://www.plz-dont-hack.io"
+    podcastStreamUrl: "http://www.plz-dont-hack.io",
+    brandName: "The Archers",
+    publishDate: "01/01/18"
   },
   {
     podcastId: "worldCupId",
     podcastName: "World Cup",
-    podcastStreamUrl: "http://www.plz-dont-hack-goalhole.io"
+    podcastStreamUrl: "http://www.plz-dont-hack-goalhole.io",
+    brandName: "World Cup",
+    publishDate: "01/01/18"
   },
   {
     podcastId: "wimbledonId",
     podcastName: "Wimbledon",
-    podcastStreamUrl: "http://www.plz-dont-hack-tennis.io"
+    podcastStreamUrl: "http://www.plz-dont-hack-tennis.io",
+    brandName: "Wimbledon",
+    publishDate: "01/01/18"
   }
 ];
 
 bus.on("GetPodcast", function(podcastId) {
   console.log("GETTING PODCAST...");
   const podcast = podcasts.find(pc => pc.podcastId === podcastId);
+  console.log(podcast);
   if (podcast) {
     bus.emit("PodcastRetrieved", podcast);
   } else {
