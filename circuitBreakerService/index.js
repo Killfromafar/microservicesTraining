681677c16ffe415cbb4894d46bed59013cde0c9a
Ross McFadyen <ross.mcfadyen@bbc.co.uk>
2018-07-04 10:57:34 +0100

    rftdysuef

--- circuitBreakerService/index.js
+++ circuitBreakerService/index.js
@@ -2,33 +2,40 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
-app.get('/', function (req, res) {
+function makeRequest() {
+  request("http://localhost:3002", function (error, response, body) {
+    console.log(body);
 
+    if (error) {
+      circuitBreaker(response, error)
+    } else {
+      response.send(body);
+    }
+  });
+}
 
-  function circuitBreaker(response) {
-    // var circuitBreakerConfig = {
-    //   errorCount: 5,
-    //   timeToReset: 5
-    // };
-    console.log('circuit breaker code');
+function circuitBreaker(response, error) {
+  // var circuitBreakerConfig = {
+  //   errorCount: 5,
+  //   timeToReset: 5
+  // };
+  console.log('circuit breaker code');
 
-    global.circuitBreakerOpen = true;
-    res.send('Circuit breaker triggered');
-  }
+  setInterval(makeRequest, 3000);
 
+  console.log('response', response);
+  console.log('error', error);
+
+  global.circuitBreakerOpen = true;
+  // response.send('Circuit breaker triggered');
+}
+
+app.get('/', function (req, response) {
   if (global.circuitBreakerOpen) {
-    res.send('I am a clever circuit breaker');
+    response.send('I am a clever circuit breaker');
   } else {
-    console.log('in request');
-    request("http://localhost:3002", function (error, response, body) {
-      console.log(body);
-
-      if (error) {
-        circuitBreaker(error)
-      } else {
-        res.send(body);
-      }
-    });
+    console.log('Make request');
+    makeRequest();
   }
 });
 
